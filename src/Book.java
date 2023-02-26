873f7f560a1ef19c776b74998215f06281f76184
nikolaeva2 <lavegirle21@gmail.com>
2023-02-26 11:44:42 +0300

    second commit

---
+++ src/Book.java
@@ -0,0 +1,58 @@
+import java.util.Objects;
+
+public class Book {
+    private final String title;
+    private int publishingYear;
+    private final Author author;
+
+    public Book(String title, int publishingYear, Author author) {
+        this.title = title;
+        this.publishingYear = publishingYear;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublishingYear() {
+
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+
+        this.publishingYear = publishingYear;
+    }
+    public Author getAuthor(){
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: " + title + ", год издания: " + publishingYear + ", автор: " + author;
+    }
+    @Override
+    public boolean equals (Object o) {
+          if (this == o) {
+            return true;
+          }
+
+          if( o== null || getClass() !=o.getClass()) {
+            return false;
+          }
+
+           Book book= (Book) o;
+            return publishingYear == book.publishingYear && Objects.equals( title, book.title)
+                && Objects.equals(author,book.author);
+    }
+    @Override
+    public int hashCode() {
+            return Objects.hash(title, publishingYear, author);
+    }
+}
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author lermontov = new Author("Михаил ", "Лермонтов.");
+        Author tolstoy = new Author("Лев  ", "Толстой.");
+        Author tolstoy2 = new Author("Лев  ", "Толстой.");
+
+        Book mcyri = new Book("Мцыри",1863 , lermontov);
+        Book warAndPeace = new Book("Война и мир",  1824 , tolstoy);
+        System.out.println(tolstoy.equals(tolstoy2));
+
+        System.out.println(mcyri);
+        System.out.println(warAndPeace);
+
+        mcyri.setPublishingYear(1865);
+        System.out.println(mcyri.hashCode());
+        System.out.println(warAndPeace.hashCode());
+        System.out.println(tolstoy.hashCode());
+        System.out.println(tolstoy2.hashCode());
     }
 }
